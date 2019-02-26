0c3454c1908157d2a018577e5390b20f422c3478
zhangtao@allmedia-cn.com <zhangtao@allmedia-cn.com>
2019-02-26 14:52:08 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,9 @@
 function route_class() {
 	return str_replace('.', '-', Route::currentRouteName());
 }
+
+// active 添加判断
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
